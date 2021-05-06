4ce3080806ca26bc8f58ac0dc26d1c226044eca2
Animesh Kumar Singh <animesh.2001singh@gmail.com>
2021-05-06 22:27:51 +0530

    ready to be deployed

---
+++ build/precache-manifest.967ba46b4d9150c358e629b52fa419cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a20926e396934384aecee63c3ba1168",
+    "url": "/track-covid-/index.html"
+  },
+  {
+    "revision": "5c920ae8fb29cb705d0d",
+    "url": "/track-covid-/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "4efe1d3adc59ff4e2136",
+    "url": "/track-covid-/static/css/main.02729728.chunk.css"
+  },
+  {
+    "revision": "5c920ae8fb29cb705d0d",
+    "url": "/track-covid-/static/js/2.700546a4.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/track-covid-/static/js/2.700546a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4efe1d3adc59ff4e2136",
+    "url": "/track-covid-/static/js/main.b8eff1f4.chunk.js"
+  },
+  {
+    "revision": "4a69b11189a0c77fe3ba",
+    "url": "/track-covid-/static/js/runtime-main.c0bb1a64.js"
+  }
+]);
